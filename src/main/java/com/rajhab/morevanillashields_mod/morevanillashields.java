48f719afb2d649c2ae8dd6fed1a7acb97479e771
Rajhab <rajhabcurseforge@gmail.com>
2024-10-23 14:48:44 +0200

    Changelog 1.0.2-1.20.2:
    - Fixed Compatibility with older forge versions
    - Fixed Redstone Shield Particle Density
    - Fixed typo in german translation
    - Made the End Crystal Shield explosive (turned on by default. To disable go into config)
    - Made End Crystal Shield Explosion serversided

--- src/main/java/com/rajhab/morevanillashields_mod/morevanillashields.java
+++ src/main/java/com/rajhab/morevanillashields_mod/morevanillashields.java
@@ -1,6 +1,7 @@
 package com.rajhab.morevanillashields_mod;
 
 import com.mojang.logging.LogUtils;
+import com.rajhab.morevanillashields_mod.event.ShieldEventHandler;
 import com.rajhab.morevanillashields_mod.item.ModCreativeModeTabs;
 import com.rajhab.morevanillashields_mod.item.ModItems;
 import com.rajhab.morevanillashields_mod.util.ModRecipeSerializer;
@@ -30,7 +31,7 @@ public class morevanillashields
         ModItems.register(modEventBus);
         ModRecipeSerializer.register(modEventBus);
         ModCreativeModeTabs.register(modEventBus);
-
+        NeoForge.EVENT_BUS.register(ShieldEventHandler.class);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event)
