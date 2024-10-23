8c78b6d595f26320ef30f7cdf15440c9650a1a79
Rajhab <rajhabcurseforge@gmail.com>
2024-10-23 14:18:53 +0200

    Changelog 1.0.3-1.21:
    - Fixed Compatibility with older forge versions
    - Fixed Redstone Shield Particle Density
    - Fixed typo in german translation
    - Made the End Crystal Shield explosive (turned on by default. To disable go into config)
    - Made End Crystal Shield Explosion serversided

---
+++ src/main/java/com/rajhab/morevanillashields_mod/event/ShieldEventHandler.java
@@ -0,0 +1,63 @@
+package com.rajhab.morevanillashields_mod.event;
+
+import com.rajhab.morevanillashields_mod.ShieldConfig;
+import com.rajhab.morevanillashields_mod.item.ModItems;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.entity.living.LivingDamageEvent;
+
+import java.util.Random;
+
+@EventBusSubscriber
+public class ShieldEventHandler {
+
+    @SubscribeEvent
+    public static void onPlayerHurt(LivingDamageEvent.Post event) {
+
+        if (ShieldConfig.ENABLE_EXPLOSION.get()) {
+
+            if (event.getEntity() instanceof LivingEntity) {
+                LivingEntity livingEntity = event.getEntity();
+
+                if (!livingEntity.getCommandSenderWorld().isClientSide && livingEntity.isBlocking()) {
+                    ItemStack shield = livingEntity.getUseItem();
+
+                    if (shield.getItem() == ModItems.END_CRYSTAL_SHIELD.get()) {
+
+                        Random random = new Random();
+                        if (random.nextInt(30) == 0) {
+
+                            boolean explosionDestroyBlocks = ShieldConfig.EXPLOSION_DESTROY_BLOCKS.get();
+                            Level.ExplosionInteraction explosionType = explosionDestroyBlocks
+                                    ? Level.ExplosionInteraction.BLOCK
+                                    : Level.ExplosionInteraction.NONE;
+
+                            livingEntity.getCommandSenderWorld().explode(
+                                    livingEntity,                   // Exploding entity (player)
+                                    livingEntity.getX(),            // X coordinate
+                                    livingEntity.getY(),            // Y coordinate
+                                    livingEntity.getZ(),            // Z coordinate
+                                    5.0F,                           // Explosion power
+                                    explosionType                   // Does or does not destroy blocks
+                            );
+
+                            Level pLevel = livingEntity.getCommandSenderWorld();
+                            EquipmentSlot handSlot = livingEntity.getUsedItemHand() == InteractionHand.MAIN_HAND ? EquipmentSlot.MAINHAND : EquipmentSlot.OFFHAND;
+
+                            shield.hurtAndBreak(175, ((ServerLevel) pLevel), ((ServerPlayer) livingEntity),
+                                    item -> livingEntity.onEquippedItemBroken(item, handSlot)
+                            );
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
