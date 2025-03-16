d3fe4bb8757c2cca75b445d13bd8127f5422d478
Rajhab <rajhabcurseforge@gmail.com>
2025-03-16 17:51:52 +0100

    Changelog 1.0.2-1.21.1:
    - Updated to 21.1.133
    - Added Copper Shield (325 durability, no function yet)
    - Added Magma Shield (255 durability)
    - Magma Shield burns enemies on hit (5 second burn time, configurable if turned on or off)
    - Removed all tooltips except for the Redstone Shield and End Crystal Shield
    - Reworked Tooltips for End Crystal Shield and Redstone Shield
    - Tooltips now only showing when advanced tooltips are activated (F3 + H)
    - Reduced wear and tear on the shield when the end crystal shield explodes (from 175 -> 65)
    
    COMING SOON:
    - Sculk Shield (emits a sculk signal for sculk sensors (opening doors for example))
    - Copper Shield function with lightning
    - Variants for Wind Burst Shield (knocks back enemies OR very high jumps)

--- src/main/java/com/rajhab/morevanillashields_mod/ShieldConfig.java
+++ src/main/java/com/rajhab/morevanillashields_mod/ShieldConfig.java
@@ -58,7 +58,7 @@ public class ShieldConfig {
         REDSTONE_SHIELD_DENSITY = BUILDER.comment("Here you can set up the density of the redstone shield. (2500 as max bcs some of yall try to crash some games)")
                 .defineInRange("redstoneShieldDensity", 1, 1, 2500);
         MAGMA_SHIELD_DENSITY = BUILDER.comment("Here you can set up the density of the magma shield. (2500 as max bcs some of yall try to crash some games)")
-                .defineInRange("redstoneShieldDensity", 1, 1, 2500);
+                .defineInRange("magmaShieldDensity", 1, 1, 2500);
         LEATHER_SHIELD_DURABILITY = BUILDER.comment("Durability of Leather Shield. Default Durability is 196")
                 .define("leatherShieldDurability", 196);
         GOLD_SHIELD_DURABILITY = BUILDER.comment("Durability of Gold Shield. Default Durability is 322")
